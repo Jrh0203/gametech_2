4e127c166e7708c9f0bda9f8a50902b3c45600ba
John Herrick <jrh0203@cs.utexas.edu>
2018-10-03 23:17:31 -0500

    Integrating tutorial

--- Bootstrap.cpp
+++ Bootstrap.cpp
@@ -45,6 +45,33 @@ using namespace OgreBites;
 int i = 0;
 Camera* cam;
 
+class MyMotionState : public btMotionState {
+public:
+    MyMotionState(const btTransform &initialpos, Ogre::SceneNode *node) {
+        mVisibleobj = node;
+        mPos1 = initialpos;
+    }
+    virtual ~MyMotionState() {    }
+    void setNode(Ogre::SceneNode *node) {
+        mVisibleobj = node;
+    }
+    virtual void getWorldTransform(btTransform &worldTrans) const {
+        worldTrans = mPos1;
+    }
+    virtual void setWorldTransform(const btTransform &worldTrans) {
+        if(NULL == mVisibleobj) return; // silently return before we set a node
+        btQuaternion rot = worldTrans.getRotation();
+        mVisibleobj->setOrientation(rot.w(), rot.x(), rot.y(), rot.z());
+        btVector3 pos = worldTrans.getOrigin();
+        // TODO **** XXX need to fix this up such that it renders properly since this doesnt know the scale of the node
+        // also the getCube function returns a cube that isnt centered on Z
+        mVisibleobj->setPosition(pos.x(), pos.y()+5, pos.z()-5);
+    }
+protected:
+    Ogre::SceneNode *mVisibleobj;
+    btTransform mPos1;
+};
+
 class TutorialApplication
         : public ApplicationContext
         , public InputListener
@@ -58,10 +85,21 @@ public:
     bool keyReleased(const KeyboardEvent& evt);
     void frameRendered(const Ogre::FrameEvent & evt );
     bool frameStarted(const Ogre::FrameEvent & evt );
+    void createBulletSim();
     Ball* b;
     bool* keys = new bool[6];
     SceneNode* camNode;
     Physics* physicsEngine;
+
+
+    btDefaultCollisionConfiguration* collisionConfiguration;
+    btCollisionDispatcher* dispatcher;
+    btBroadphaseInterface* overlappingPairCache;
+    btSequentialImpulseConstraintSolver* solver;
+    btDiscreteDynamicsWorld* dynamicsWorld;
+    btCollisionShape* groundShape;
+    btAlignedObjectArray<btCollisionShape*> collisionShapes;
+    SceneNode *boxNode;
 };
 
 
@@ -74,6 +112,89 @@ TutorialApplication::TutorialApplication()
 TutorialApplication::~TutorialApplication()
 {
 }
+
+
+
+void TutorialApplication::createBulletSim(void) {
+        ///collision configuration contains default setup for memory, collision setup. Advanced users can create their own configuration.
+        collisionConfiguration = new btDefaultCollisionConfiguration();
+
+        ///use the default collision dispatcher. For parallel processing you can use a diffent dispatcher (see Extras/BulletMultiThreaded)
+        dispatcher = new    btCollisionDispatcher(collisionConfiguration);
+
+        ///btDbvtBroadphase is a good general purpose broadphase. You can also try out btAxis3Sweep.
+        overlappingPairCache = new btDbvtBroadphase();
+
+        ///the default constraint solver. For parallel processing you can use a different solver (see Extras/BulletMultiThreaded)
+        solver = new btSequentialImpulseConstraintSolver;
+
+        dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher,overlappingPairCache,solver,collisionConfiguration);
+        dynamicsWorld->setGravity(btVector3(0,-10,0));
+
+        ///create a few basic rigid bodies
+        // start with ground plane, 1500, 1500
+        btCollisionShape* groundShape = new btBoxShape(btVector3(btScalar(1500.),btScalar(1.),btScalar(1500.)));
+
+        collisionShapes.push_back(groundShape);
+
+        btTransform groundTransform;
+        groundTransform.setIdentity();
+        groundTransform.setOrigin(btVector3(0,-2,0));
+
+        {
+            btScalar mass(0.);
+
+            //rigidbody is dynamic if and only if mass is non zero, otherwise static
+            bool isDynamic = (mass != 0.f);
+
+            btVector3 localInertia(0,0,0);
+            if (isDynamic)
+                groundShape->calculateLocalInertia(mass,localInertia);
+
+            // lathe - this plane isnt going to be moving so i dont care about setting the motion state
+            //using motionstate is recommended, it provides interpolation capabilities, and only synchronizes 'active' objects
+            btDefaultMotionState* myMotionState = new btDefaultMotionState(groundTransform);
+            btRigidBody::btRigidBodyConstructionInfo rbInfo(mass,myMotionState,groundShape,localInertia);
+            btRigidBody* body = new btRigidBody(rbInfo);
+
+            //add the body to the dynamics world
+            dynamicsWorld->addRigidBody(body);
+        }
+
+
+        {
+            //create a dynamic rigidbody
+
+            btCollisionShape* colShape = new btBoxShape(btVector3(1,1,1));
+  //            btCollisionShape* colShape = new btSphereShape(btScalar(1.));
+            collisionShapes.push_back(colShape);
+
+            /// Create Dynamic Objects
+            btTransform startTransform;
+            startTransform.setIdentity();
+
+            btScalar    mass(1.f);
+
+            //rigidbody is dynamic if and only if mass is non zero, otherwise static
+            bool isDynamic = (mass != 0.f);
+
+            btVector3 localInertia(0,0,-1.0);
+            if (isDynamic)
+                colShape->calculateLocalInertia(mass,localInertia);
+
+                startTransform.setOrigin(btVector3(0,250,0));
+                // *** give it a slight twist so it bouncees more interesting
+                startTransform.setRotation(btQuaternion(btVector3(1.0, 1.0, 0.0), 0.6));
+
+                //using motionstate is recommended, it provides interpolation capabilities, and only synchronizes 'active' objects
+                //btDefaultMotionState* myMotionState = new btDefaultMotionState(startTransform);
+                MyMotionState* motionState = new MyMotionState(startTransform, boxNode);
+                btRigidBody::btRigidBodyConstructionInfo rbInfo(mass,motionState,colShape,localInertia);
+                btRigidBody* body = new btRigidBody(rbInfo);
+
+                dynamicsWorld->addRigidBody(body);
+        }
+    }
 
 
 void TutorialApplication::setup()
